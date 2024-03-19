2dffac3d37b5ddd4e0fda2b85d37dd9464cf9576
Janindu Gayanga <83686809+jani358@users.noreply.github.com>
2024-03-20 01:17:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// Middleware
+app.use(express.json()); // Parse incoming JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
 
 // DB Connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-
 });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('successfully database connected');
+  console.log('Successfully connected to the database');
 });
 
-
+// Routes
 const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes); // Use user routes for URLs starting with '/api/users'
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
