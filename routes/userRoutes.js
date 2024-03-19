10b82694db78386ba694d9f35f1e359614ef2ddb
Janindu Gayanga <83686809+jani358@users.noreply.github.com>
2024-03-20 01:16:09 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// Route to create a new user
 router.post('/', userController.createUser);
+
+// Route to get all users
 router.get('/', userController.getAllUsers);
+
+// Route to get a user by ID
 router.get('/:id', userController.getUserById);
+
+// Route to update a user by ID
 router.put('/:id', userController.updateUser);
+
+// Route to delete a user by ID
 router.delete('/:id', userController.deleteUser);
 
+// Export the router to be used in the main application file
 module.exports = router;
